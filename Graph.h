7548d3c1d15a591a94235931df86d3d06718c242
Qwerty7310 <daniyl.2004@yandex.ru>
2024-10-25 01:02:33 +0300

    renamed Graph.h from Edge.h and add Graph.cpp

---
+++ Graph.h
@@ -0,0 +1,21 @@
+#ifndef LAB_1_BELLMAN_FORD_JOHNSON__GRAPH_H
+#define LAB_1_BELLMAN_FORD_JOHNSON__GRAPH_H
+
+#include <cstdint>
+#include <fstream>
+#include <vector>
+
+#define INF INT16_MAX
+
+struct Edge {
+    int from;
+    int to;
+    int16_t cost;
+};
+
+std::vector<Edge> readGraph(std::ifstream &file, int16_t &n);
+void readGraph(std::ifstream &file, int16_t &n, int ***matrix);
+std::vector<Edge> readGraph2(int16_t matrix[10][10], int16_t &n);
+void readGraph3(int matrix[10][10], int16_t &n, int ***matrix1);
+
+#endif  // LAB_1_BELLMAN_FORD_JOHNSON__GRAPH_H

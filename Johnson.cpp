6dcbfc182935766e8c1805a56fbe0f219380b1c4
Qwerty7310 <daniyl.2004@yandex.ru>
2024-10-26 03:07:52 +0300

    update Johnson

--- Johnson.cpp
+++ Johnson.cpp
@@ -10,15 +10,15 @@
 using namespace std;
 
 int Johnson(int n, int **matr_input, int **res) {
-    // Добавление новой вершины
+    // Создаем новую матрицу с дополнительной вершиной
     int **new_matrix = new int *[n + 1];
     for (int i = 0; i < n + 1; i++) new_matrix[i] = new int[n + 1];
 
-    for (int u = 0; u < n; u++) {
-        for (int v = 0; v < n; v++) {
-            new_matrix[u][v] = matr_input[u][v];
-        }
-    }
+    // Копируем элементы из старой матрицы
+    for (int u = 0; u < n; u++)
+        for (int v = 0; v < n; v++) new_matrix[u][v] = matr_input[u][v];
+
+    // Добавляем новую вершину
     for (int v = 0; v < n + 1; v++) new_matrix[n][v] = 0;
     for (int u = 0; u < n; u++) new_matrix[u][n] = INF;
 
@@ -29,35 +29,65 @@ int Johnson(int n, int **matr_input, int **res) {
         return -1;
     }
     // Изменяем вес ребер, чтобы они были неотрицательными
-    for (int u = 0; u < n; u++) {
-        for (int v = 0; v < n; v++) {
-            new_matrix[u][v] = new_matrix[u][v] + dist[u] - dist[v];
-        }
-    }
+    for (int u = 0; u < n; u++)
+        for (int v = 0; v < n; v++) new_matrix[u][v] = new_matrix[u][v] + dist[u] - dist[v];
 
     delete[] prev;
     prev = new int[n];
-    for (int i = 0; i < n; i++) {
-        Dijkstra(n, i, new_matrix, res[i], prev);
-    }
+    for (int i = 0; i < n; i++) Dijkstra(n, i, new_matrix, res[i], prev);
 
     // Находим кратчайшие расстояния для исходного графа
-    for (int u = 0; u < n; u++) {
-        for (int v = 0; v < n; v++) {
-            res[u][v] = res[u][v] - dist[u] + dist[v];
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << 0 << " - " << i << ": ";
-        if (res[0][i] == INF)
-            cout << "∞" << endl;
-        else
-            cout << res[0][i] << endl;
-    }
+    for (int u = 0; u < n; u++)
+        for (int v = 0; v < n; v++) res[u][v] = res[u][v] - dist[u] + dist[v];
 
     delete[] dist;
     delete[] prev;
 
     return 0;
+}
+
+void eccentricity(int n, int **matrix, int *res) {
+    for (int i = 0; i < n; i++) {
+        int max = -INF;
+        for (int j = 0; j < n; j++)
+            if (matrix[i][j] != INF && matrix[i][j] > max) max = matrix[i][j];
+        res[i] = max;
+    }
+}
+
+int radius(int n, int **matrix) {
+    int *arr_eccentricity = new int[n]; // эксцентриситеты вершин
+    eccentricity(n, matrix, arr_eccentricity);
+    int min = arr_eccentricity[0];
+    for (int i = 1; i < n; i++)
+        if (arr_eccentricity[i] < min) min = arr_eccentricity[i];
+
+    delete[] arr_eccentricity;
+    return min;
+}
+
+int diameter(int n, int **matrix) {
+    int *arr_eccentricity = new int[n]; // эксцентриситеты вершин
+    eccentricity(n, matrix, arr_eccentricity);
+    int max = arr_eccentricity[0];
+    for (int i = 1; i < n; i++)
+        if (arr_eccentricity[i] > max) max = arr_eccentricity[i];
+
+    delete[] arr_eccentricity;
+    return max;
+}
+
+void vertices(int n, int **matrix, vector<int> &central, vector<int> &peripheral) {
+    int rad = radius(n, matrix); // радиус графа
+    int diam = diameter(n, matrix); // диаметр графа
+
+    int *arr_eccentricity = new int[n]; // эксцентриситеты вершин
+    eccentricity(n, matrix, arr_eccentricity);
+
+    for (int i = 0; i < n; i++) {
+        if (arr_eccentricity[i] == rad) central.push_back(i);
+        if (arr_eccentricity[i] == diam) peripheral.push_back(i);
+    }
+
+    delete[] arr_eccentricity;
 }
--- Johnson.h
+++ Johnson.h
@@ -1,6 +1,37 @@
-#ifndef LAB_1_BELLMAN_FORD_JOHNSON__JOHNSON_H
-#define LAB_1_BELLMAN_FORD_JOHNSON__JOHNSON_H
+#ifndef LAB_1_BELLMAN_FORD_JOHNSON_JOHNSON_H
+#define LAB_1_BELLMAN_FORD_JOHNSON_JOHNSON_H
 
+#include <vector>
+
+/// \param n количество вершин
+/// \param matr_input матрица смежности
+/// \param res матрица кратчайших расстояний
+/// \return -1 - присутствует цикл с отрицательным весом, 0 - в другом случае
 int Johnson(int n, int **matr_input, int **res);
 
-#endif  // LAB_1_BELLMAN_FORD_JOHNSON__JOHNSON_H
+/// Нахождение эксцентриситетов вершин
+/// \param n количество вершин
+/// \param matrix матрица кратчайших расстояний
+/// \param res массив результатов
+void eccentricity(int n, const int **matrix, int *res);
+
+/// Радиус графа
+/// \param n количество вершин
+/// \param matrix матрица смежности
+/// \return радиус графа
+int radius(int n, int **matrix);
+
+/// Диаметр графа
+/// \param n количество вершин
+/// \param matrix матрица смежности
+/// \return диаметр графа
+int diameter(int n, int **matrix);
+
+/// Центральные и периферийные вершины
+/// \param n количество вершин
+/// \param matrix матрица смежности
+/// \param central вектор центральных вершин
+/// \param peripheral вектор периферийных вершин
+void vertices(int n, int **matrix, std::vector<int> &central, std::vector<int> &peripheral);
+
+#endif  // LAB_1_BELLMAN_FORD_JOHNSON_JOHNSON_H
